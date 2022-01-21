7eb993ff432bf20674da4c11efe1e312eaad6fce
Sanmitr564 <generic39542@gmail.com>
2022-01-21 14:04:17 -0800

    initial commit

---
+++ FirstDrawing.java
@@ -0,0 +1,70 @@
+import com.badlogic.gdx.ApplicationAdapter; 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.viewport.*;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer; 
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Rectangle; 
+import com.badlogic.gdx.math.Circle; 
+import com.badlogic.gdx.Input.Keys; 
+import com.badlogic.gdx.math.Vector2; 
+import com.badlogic.gdx.math.MathUtils; 
+import com.badlogic.gdx.math.Intersector; 
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.utils.*;
+import com.badlogic.gdx.*; 
+
+//NOTE: Always reset the JVM before compiling (it is the small loop arrow in the
+//bottom right corner of the project window)!! 
+
+public class FirstDrawing extends ApplicationAdapter 
+{
+    private OrthographicCamera camera; //the camera to our world
+    private Viewport viewport; //maintains the ratios of your world
+    private ShapeRenderer renderer; //used to draw textures and fonts 
+    private BitmapFont font; //used to draw fonts (text)
+    private SpriteBatch batch; //also needed to draw fonts (text)
+
+    @Override//called once when we start the game
+    public void create(){
+
+        camera = new OrthographicCamera(); 
+        viewport = new FitViewport(GLOBAL.WORLD_WIDTH, GLOBAL.WORLD_HEIGHT, camera); 
+        renderer = new ShapeRenderer(); 
+        font = new BitmapFont(); 
+        batch = new SpriteBatch();//if you want to use images instead of using ShapeRenderer 
+
+    }
+
+    @Override//called 60 times a second
+    public void render(){
+        preRender();
+        
+    }
+    @Override
+    public void resize(int width, int height){
+        viewport.update(width, height, true); 
+    }
+
+    @Override
+    public void dispose(){
+        renderer.dispose(); 
+        batch.dispose(); 
+    }
+    
+    public void preRender(){
+        viewport.apply(); 
+
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        float delta = Gdx.graphics.getDeltaTime();//1/60 
+
+        //draw everything on the screen
+        renderer.setProjectionMatrix(viewport.getCamera().combined);
+    }
+}
---
+++ GLOBAL.java
@@ -0,0 +1,4 @@
+public class GLOBAL{
+    public static int WORLD_WIDTH = 600;
+    public static int WORLD_HEIGHT = 800;
+}
